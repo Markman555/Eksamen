5fd624586d429a1958ce8530077c6dccbb900652
Markman555 <markflemminga@gmail.com>
2024-04-03 15:41:10 +0200

    function for displaying 50 pokemon fetched

---
+++ script.js
@@ -0,0 +1,50 @@
+const pokemonContainer = document.getElementById("Pk-Container");
+const select = document.getElementById("type-selection");
+let pokemonList;
+
+async function fetchPokemon() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+    const pokemons = await response.json();
+    pokemonList = pokemons.results;
+    displayPokemons();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function displayPokemons() {
+  pokemonList.forEach(async (pokemon) => {
+    const response = await fetch(pokemon.url); // Fetch individuelle pokemon url
+    const pokemonData = await response.json();
+
+    const pokemonCard = document.createElement("div");
+    const catchPokemon = document.createElement("button");
+    const deletePokemon = document.createElement("button");
+    const editPokemon = document.createElement("button");
+
+    const pokemonType = pokemonData.types //types er et array som inneholder to objekter, derfor bruker jeg map metoden på det
+      .map((type) => type.type.name)
+      .join(", ");
+
+    pokemonCard.innerHTML = `<img src=${pokemonData.sprites.front_default} /> <h3>${pokemonData.name}</h3> <p>${pokemonType}</p>`;
+    pokemonCard.style.textAlign = "center";
+    pokemonContainer.append(pokemonCard);
+
+    catchPokemon.textContent = "Lagre";
+    deletePokemon.textContent = "Fjern";
+    editPokemon.textContent = "Rediger";
+
+    catchPokemon.style.marginLeft = "5px";
+    deletePokemon.style.marginLeft = "5px";
+    editPokemon.style.marginLeft = "5px";
+
+    pokemonCard.append(catchPokemon);
+    pokemonCard.append(deletePokemon);
+    pokemonCard.append(editPokemon);
+  });
+}
+
+
+
+fetchPokemon();
